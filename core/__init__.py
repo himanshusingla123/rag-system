dbaf2683360c4448516e7e67be6de8708aabfeb7
himanshusingla123 <95504579+himanshusingla123@users.noreply.github.com>
2025-08-16 02:58:27 +0530

    first commit

---
+++ core/__init__.py
@@ -0,0 +1,12 @@
+
+"""
+Core components for the News RAG System
+"""
+
+from .rag_system import NewsRAGSystem
+from .database_manager import DatabaseManager
+
+__all__ = [
+    'NewsRAGSystem',
+    'DatabaseManager'
+]
